59b24170c19c01c282bd3d34c10004f8b686624d
theryanclausen <theryanclausen@gmail.com>
2019-02-18 13:50:52 -0700

    day one mvp

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -6,23 +6,27 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        
-
-
-
+        for j in range(i, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
         # TO-DO: swap
-
-
-
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort( arr ):
-
+    i = 1
+    while i < len(arr):
+        if i == 0:
+            i = 1
+        if arr[i] < arr[i - 1]:
+            arr[i], arr[i-1] = arr[i-1], arr[i]
+            i -= 1
+        else:
+            i += 1
     return arr
-
 
 # STRETCH: implement the Bubble Sort function below
 def bubble_sort( arr ):

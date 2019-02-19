321c57a653c947a8ba3448e5bcd7e963bd4a3267
theryanclausen <theryanclausen@gmail.com>
2019-02-19 14:13:11 -0700

    did some search stretches. Lots of errors in code. Existing code doesnt match style guidelines. discouraging. please fix.

--- project/searching.py
+++ project/searching.py
@@ -2,22 +2,33 @@
 def linear_search(arr, target):
   
   # TO-DO: add missing code
+  for i in range(len(arr)):
+    if arr[i] == target:
+      return i
 
-   return -1   # not found
-
+  return -1   # not found
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
 
   if len(arr) == 0:
     return -1 # array empty
-    
   low = 0
   high = len(arr)-1
-
+  
   # TO-DO: add missing code
+  while high > low:
+    mid = (high - low) // 2
+    print(mid, high, low)
+    if arr[mid] == target:
+      return mid
+    if arr[mid] > target:
+      high = mid
+    else:
+      low = mid
 
   return -1 # not found
+
 
 
 # STRETCH: write a recursive implementation of Binary Search 
